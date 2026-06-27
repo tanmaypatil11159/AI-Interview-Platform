ba64663200d7b15158f411abece7e487bbc57a49
Tanmay Patil <itanmaypatil@gmail.com>
2026-06-27 15:30:36 +0530

    multer

--- server/middleware/multer.js
+++ server/middleware/multer.js
@@ -13,7 +13,10 @@ const storage = multer.diskStorage({
     }
 })
 
+
 export const upload = multer({
-    storage,
-    limits: {fileSize: 5 * 1024 * 1024} // 5 MB limit
-})
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 5 * 1024 * 1024, // 5 MB
+  },
+});
